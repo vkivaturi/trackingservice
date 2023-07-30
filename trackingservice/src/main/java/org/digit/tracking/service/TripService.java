24e7d51f64df54c7ba4c372940ccbd689cec3c38
vkivaturi <vijay.ivaturi@gmail.com>
2023-07-30 15:48:04 +0530

    Trip API basic implementation

---
+++ trackingservice/src/main/java/org/digit/tracking/service/TripService.java
@@ -0,0 +1,69 @@
+package org.digit.tracking.service;
+
+import org.openapitools.model.Operator;
+import org.openapitools.model.Trip;
+import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TripService {
+    public List<Trip> getTripsBySearch(Trip trip) {
+        //TODO - Mock list of alerts. Replace with database call
+        return fetchTripsFromDB(trip);
+    }
+
+    private List<Trip> fetchTripsFromDB(Trip searchTrip) {
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+
+        List<Trip> triplist = new ArrayList<Trip>();
+        //Add first Trip
+        Trip trip = new Trip();
+        trip.setRouteId("ROUTE_ID_1");
+        trip.setServiceCode("SANIT-FSCS");
+
+        Operator operator = new Operator();
+        operator.setId("driver11");
+        operator.setEmail("test@yahoo.com");
+        operator.setName("Driver kumar");
+        operator.setContactNumber("9999999999");
+        operator.setVehicleNumber("KA 11 2312");
+        trip.setOperator(operator);
+
+        trip.setStatus(Trip.StatusEnum.IN_PROGRESS);
+        trip.setPlannedStartTime(OffsetDateTime.parse("2023-07-30T19:20:17.555Z"));
+        trip.setPlannedEndTime(OffsetDateTime.now());
+        trip.setActualStartTime(OffsetDateTime.now());
+        trip.setActualEndTime(OffsetDateTime.now());
+
+        triplist.add(trip);
+
+        //Add second trip
+        trip = new Trip();
+        trip.setRouteId("ROUTE_ID_2");
+        trip.setServiceCode("SANIT-MOSQ");
+
+        operator = new Operator();
+        operator.setId("driver3345");
+        operator.setEmail("nothing@yahoo.com");
+        operator.setName("Driver rao");
+        operator.setContactNumber("1111111111");
+        operator.setVehicleNumber("TS 33 4312");
+        trip.setOperator(operator);
+
+        trip.setStatus(Trip.StatusEnum.IN_PROGRESS);
+//        trip.setPlannedStartTime(OffsetDateTime.parse("2023-07-30T19:20:17.555Z"));
+//        trip.setPlannedEndTime(OffsetDateTime.parse("2023-07-30T20:20:17.555Z"));
+//        trip.setActualStartTime(OffsetDateTime.parse("2023-07-30T20:20:17.555Z"));
+//        trip.setActualEndTime(OffsetDateTime.parse("2023-07-30T21:20:17.555Z"));
+
+        triplist.add(trip);
+
+        return triplist;
+    }
+
+}
+
--- trackingservice/src/main/java/org/digit/tracking/util/JsonUtil.java
+++ trackingservice/src/main/java/org/digit/tracking/util/JsonUtil.java
@@ -2,10 +2,12 @@ package org.digit.tracking.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JsonUtil {
     public static String getJsonFromObject(Object obj){
         ObjectMapper converter = new ObjectMapper();
+        converter.registerModule(new JavaTimeModule());
         try {
             return converter.writeValueAsString(obj);
         } catch (JsonProcessingException e) {
