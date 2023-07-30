4f539cf9716a6f51b8edcd7e9be1fb2011bfed41
Nitesh Singh <kr.nitesh010@gmail.com>
2023-07-30 19:10:11 +0530

    Initial commit for trip monitoring service

---
+++ tripmonitoringservice/src/main/java/org/digit/tripmonitoring/service/TripmonitoringserviceApplication.java
@@ -0,0 +1,13 @@
+package org.digit.tripmonitoring.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripmonitoringserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripmonitoringserviceApplication.class, args);
+	}
+
+}
---
+++ tripmonitoringservice/src/test/java/org/digit/tripmonitoring/service/TripmonitoringserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.digit.tripmonitoring.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripmonitoringserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
