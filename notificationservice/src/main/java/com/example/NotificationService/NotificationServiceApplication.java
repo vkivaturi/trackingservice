a5a88b4074fa6b11459d8a51809b31d528e5eaa0
vastavLaksh <srivastavalaksh2020@gmail.com>
2023-08-03 14:59:06 +0530

    Intial commit project structure

---
+++ notificationservice/src/main/java/com/example/NotificationService/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.NotificationService1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationService1Application.class, args);
+	}
+
+}
---
+++ notificationservice/src/test/java/com/example/NotificationService/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NotificationService1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
