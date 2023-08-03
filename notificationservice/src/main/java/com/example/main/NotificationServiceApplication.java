4af47a45b320f822c5681854c1885e62bde75485
vastavLaksh <srivastavalaksh2020@gmail.com>
2023-08-03 16:53:15 +0530

    directories added

--- notificationservice/src/main/java/com/example/main/NotificationServiceApplication.java
+++ notificationservice/src/main/java/com/example/main/NotificationServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotificationService1Application {
+public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NotificationService1Application.class, args);
+		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
 }
